44a4ef00edba3abad1a30c6ec89fd739f330c5f5
Curtis Thompson <curtiswaynethompson@gmail.com>
2021-01-08 21:47:50 +0000

    Notes index (public)

--- del.py
+++
@@ -1,7 +0,0 @@
-import os
-
-PATH = '.\\public_html\\notes'
-html_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(PATH) for f in filenames if os.path.splitext(f)[1] == '.php']
-
-for f in html_files:
-	os.remove(f)
