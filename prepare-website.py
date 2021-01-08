8bc29d17adafc2765478c870bcef027689084921
Curtis Thompson <curtiswaynethompson@gmail.com>
2021-01-08 21:44:27 +0000

    Notes template

--- prepare-website.py
+++ prepare-website.py
@@ -19,26 +19,21 @@ def write_file_contents(file, data):
     fin.close()
 
 
-def get_header(template):
+def get_tag(template, tag):
     try:
-        return re.findall('<!HEADCONTENT!>[\s\S]*<!HEADCONTENT!>', template)[0].replace('<!HEADCONTENT!>', '')
-    except:
-        return ''
-
-def get_body(template):
-    try:
-        return re.findall('<!BODYCONTENT!>[\s\S]*<!BODYCONTENT!>', template)[0].replace('<!BODYCONTENT!>', '')
+        return re.findall(tag + '[\s\S]*' + tag, template)[0].replace(tag, '')
     except:
         return ''
 
 
 def fill_file(file, template):
     data = get_file_contents(file)
-    header = get_header(data)
-    body = get_body(data)
     
-    results = re.sub('<!HEADCONTENT!>', header, template)
-    results = re.sub('<!BODYCONTENT!>', body, results)
+    results = template
+    for tag in ['<!HEADCONTENT!>', '<!BODYCONTENT!>', '<!NOTETITLE!>'
+                '<!NOTEAUTHOR!>', '<!NOTEDATE!>', '<!NOTETOPIC!>',
+                '<!NOTEOTHERS!>']:
+        results = re.sub(tag, get_tag(data, tag), results)
     return results
 
 
@@ -49,17 +44,25 @@ FOOTER = get_file_contents('templates/footer.html').replace('\n', '\n\t\t')
 
 # Get the templates
 GENERIC_TEMPLATE = get_file_contents('templates/general.html')
+NOTES_TEMPLATE = get_file_contents('templates/notes.html')
 
-# Fill in the template with common parts
+# Fill in the generic template with common parts
 PREFILLED_TEMPLATE = re.sub('<!COREHEADER!>', COREHEADER, GENERIC_TEMPLATE)
 PREFILLED_TEMPLATE = re.sub('<!MENU!>', MENU, PREFILLED_TEMPLATE)
 PREFILLED_TEMPLATE = re.sub('<!FOOTER!>', FOOTER, PREFILLED_TEMPLATE)
 
+# Fill in the notes template with common parts
+PREFILLED_NOTES = re.sub('<!COREHEADER!>', COREHEADER, NOTES_TEMPLATE)
+PREFILLED_NOTES = re.sub('<!MENU!>', MENU, PREFILLED_NOTES)
+PREFILLED_NOTES = re.sub('<!FOOTER!>', FOOTER, PREFILLED_NOTES)
+
 # Go through files and merge with templates, then output
 for file in html_files:
+    print(file)
     if '\\notes\\' not in file:
-        print(file)
         data = fill_file(file, PREFILLED_TEMPLATE)
-        new_path = file.replace('\\no_public_html\\', '\\public_html\\')
-        write_file_contents(new_path, data)
+    else:
+        data = fill_file(file, PREFILLED_NOTES)
     
+    new_path = file.replace('\\no_public_html\\', '\\public_html\\')
+    write_file_contents(new_path, data)
